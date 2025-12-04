59a559473cffc6c1e9e5b901bbda5e20306f4024
Freddy <20foakley@gmail.com>
2025-12-04 14:08:26 -0500

    a

--- fars_exploratory.py
+++ fars_exploratory.py
@@ -16,19 +16,6 @@ def main():
     print(person_df['REST_USE'].unique()) 
 
     print(person_df[ (person_df['REST_USE'] == 20) & (person_df['PER_TYP'].isin(1,2)) & (person_df['INJ_SEV']==4) ])
-    
-
-
-
-
-
-
-    # ---------------------------------------------------------
-    # Step 1 - we extract all Front-to-Front crashes only
-    # ---------------------------------------------------------
-    # accident_df = accident_df[accident_df["MAN_COLL"]==2]                  # encoded value of 2 means Front-to-Front
-
-
 
 if __name__ == "__main__":
     main()
